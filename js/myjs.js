c07482371ea0ba69c78a87e2048700fe8002e458
Eric Yew <eric9yew@gmail.com>
2018-03-24 20:22:14 +0800

    updated js

---
+++ js/myjs.js
@@ -0,0 +1,24 @@
+function sum(a,b) {
+        var n1 = parseInt(document.getElementById(a).value);
+        var n2 = parseInt(document.getElementById(b).value);
+        	
+        var sum = n1 + n2;
+
+  		var s= document.getElementById("s1");
+  		s.innerHTML = sum;
+		
+}
+
+function calcTotalSleep(a,b,c) {
+
+		//input
+        var sleep_pd = parseInt(document.getElementById(a).value);
+        var death = parseInt(document.getElementById(b).value);
+        var current = parseInt(document.getElementById(c).value);	
+
+        //totol hour of sleep formula
+        var total_sleep = ((sleep_pd*365)*(death-current))/(365*24);
+
+  		var s = document.getElementById("s1");
+  		s.innerHTML = total_sleep;
+}
